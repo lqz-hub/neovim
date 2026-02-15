5b4d89661343eb4ec0d3eda8748f7dc3c7ba0645
liquzhi <liquzhi@scu.edu.cn>
2026-02-16 00:21:50 +0800

    change to multiple cursors

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,51 +1,37 @@
 return {
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true -- use opts = {} for passing setup options
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    },
-    {
-        "smoka7/multicursors.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            'nvimtools/hydra.nvim',
-        },
-        opts = {},
-        cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-        keys = {
-            {
-                mode = { 'v', 'n' },
-                '<Leader>m',
-                '<cmd>MCstart<cr>',
-                desc = 'Create a selection for selected text or word under the cursor',
-            },
-        },
-    },
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require('bufferline').setup({
-                options = {
-                    custom_filter = function(buf_number, _)
-                        if vim.bo[buf_number].filetype == "qf" then return false end
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true, -- use opts = {} for passing setup options
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  { "terryma/vim-multiple-cursors" },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          custom_filter = function(buf_number, _)
+            if vim.bo[buf_number].filetype == "qf" then
+              return false
+            end
 
-                        return true
-                    end,
-                }
-            })
-        end
-    },
-    { "preservim/nerdtree" },
+            return true
+          end,
+        },
+      })
+    end,
+  },
+  { "preservim/nerdtree" },
 }
